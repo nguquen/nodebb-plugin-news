3abf0a40c3a75dabbc9ed360ccd5ddb8efad74c9
nguquen <nguquen@botbie.com>
2016-06-04 13:21:08 +0700

    fix load image

--- public/lib/main.js
+++ public/lib/main.js
@@ -5,8 +5,9 @@
 $('document').ready(function() {
 	$(window).on('action:ajaxify.end', function(err, data) {
 		console.log(JSON.stringify(data));
-		if (data.url === '' || data.url.match(/^news\//)) {
+		if (data.url === '' || data.url.match(/^news/)) {
 			$(window).trigger('action:topic.loaded');
+			$(window).trigger('action:news.loaded');
 		}
 	});
 
@@ -25,6 +26,20 @@ $('document').ready(function() {
 		socket.emit('plugins.News.toggleMarkNews', {tid: tid}, function(err, data) {
 			app.alertSuccess(data.isNews ? 'Topic has been marked as a news' : 'Topic is now a regular thread');
 			ajaxify.refresh();
+		});
+	}
+
+	require(['components'], function(components) {
+		$(window).on('action:news.loaded', function() {
+			loadImages(components);
+		});
+	});
+
+	function loadImages(components) {
+		var images = components.get('post/content').find('img:not(.not-responsive)');
+
+		images.each(function() {
+			$(this).attr('data-state', 'loaded');
 		});
 	}
 });
